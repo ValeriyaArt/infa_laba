7462af7393b61161b2c29e4a319738d823b3e8f6
ValeriyaArt <32551040+ValeriyaArt@users.noreply.github.com>
2018-06-02 22:42:00 +0300

    Update edit.php

--- edit.php
+++ edit.php
@@ -3,7 +3,7 @@
  
     $link = mysqli_connect($host, $user, $password, $database) 
     or die ("Обнаружена ошибка в подключении к базе данных. Будь умницей, исправь, пока не поздно" . mysqli_error());
-    $query = "SELECT order_cost FROM sales";
+    $query = "SELECT id_client FROM sales";
     $result = mysqli_query($link, $query);
     
 ?>
@@ -14,26 +14,20 @@
 <input type='hidden' name='id_order' value='<?=@$_GET['id_order']?>'>
 <table border='1'>
 <tr>
-<th>id_order</th>
-<th>id_product</th>
-<th>id_client</th>
 <th>order_quantity</th>
 <th>departure_date</th>
 <th>order_cost</th>
 </tr>
-<tr>
-<td><input type='int' name='id_order' value='<?=@$_GET['id_order']?>'></td>
-<td><input type='int' name='id_product' value='<?=@$_GET['id_product']?>'></td>
-<td><input type='int' name='id_client' value='<?=@$_GET['id_client']?>'></td>
 <td><input type='int' name='order_quantity' value='<?=@$_GET['order_quantity']?>'></td>
 <td><input type='date' name='departure_date' value='<?=@$_GET['departure_date']?>'></td>
-<td><select name='order_cost'>
+<td><input type='text' name='order_cost' value='<?=@$_GET['order_cost']?>'></td>
+<td><select name='id_client'>
             <?php
             while($row = mysqli_fetch_assoc($result)) {
-                if ($row['order_cost'] == $_GET['sales'])
-                    echo "<option selected value='" . $row['order_cost'] . "'>" . $row['order_cost'] . "</option>";
+                if ($row['id_client'] == $_GET['sales'])
+                    echo "<option selected value='" . $row['id_client'] . "'>" . $row['id_client'] . "</option>";
                 else
-                    echo "<option value='" . $row['order_cost'] . "'>" . $row['order_cost'] . "</option>";
+                    echo "<option value='" . $row['id_client'] . "'>" . $row['id_client'] . "</option>";
             }
             ?>
         </select></td>
@@ -43,4 +37,3 @@
 </form>
 </body>
 </html>
-
