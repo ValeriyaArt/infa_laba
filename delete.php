94f0f991d84c0c2ad555b3bc0427296e0b682ba1
ValeriyaArt <32551040+ValeriyaArt@users.noreply.github.com>
2018-06-02 22:43:03 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,19 +1,13 @@
 <?php
 require_once 'connection.php';
-
 $link = mysqli_connect($host, $user, $password, $database) 
     or die ("Обнаружена ошибка в подключении к базе данных. Будь умницей, исправь, пока не поздно" . mysqli_error());
-
-$id = $_GET['id_order'];
-
+$id_order = $_GET['id_order'];
 $sql = "SET foreign_key_checks = 0";
 $result = mysqli_query($link, $sql);
-
 $query = "DELETE FROM sales
-	WHERE id_order ='$id_order'";
+    WHERE id_order ='$id_order'";
 $result = mysqli_query($link, $query);
-
-
 ?>
 
 <html>
